4d4011fa10c77de0dfdbf3db61ea81792daef7f0
ingridkodehode <112859721+ingridkodehode@users.noreply.github.com>
2022-12-05 12:17:45 +0100

    Create julbot.sh

---
+++ .github/workflows/julbot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+green="\033[0;32m"
+red="\033[0;31m"
+nc="\033[0m"
+
+nr1=3
+nr2=5
+
+for x in {1..100};
+do
+  if (( $x % $nr1 == 0 ))
+   then
+     echo -e "${green}HO${nc}"
+     sleep 0.3
+     if (( $x % $nr2 == 0 ))
+       then
+         echo -e "${green}HO${red}HO${nc}"
+         sleep 0.3
+   fi
+ elif (( $x % $nr2 == 0 ))
+   then
+     echo -e "${red}HO${nc}"
+     sleep 0.3
+ else
+     echo "$x"
+     sleep 0.3
+   fi
+done
